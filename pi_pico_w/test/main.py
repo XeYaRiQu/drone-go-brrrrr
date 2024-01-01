177edb653ca78127e41df4afa89fba21ba696f2b
XeYaRiQu <142389628+XeYaRiQu@users.noreply.github.com>
2024-01-02 01:28:33 +0800

    Add PID calculations

--- pi_pico_w/test/main.py
+++ pi_pico_w/test/main.py
@@ -2,6 +2,7 @@
 """
 Avoid using classes and minimise unnecessary function calls
 to increase processing speed in the main flight control loop.
+Try to replace integer division with float multiplication.
 """
 
 ##### Imports #####
@@ -15,8 +16,30 @@ from machine import UART, Pin, PWM, freq, I2C
 gyro_range:int = 500 # 250, 500, 1000, 2000 dps (IMU_REG_GYRO_CONFIG[3:4])
 acce_range:int = 4   # 2, 4, 8, 16 g (IMU_REG_ACCE_CONFIG[3:4])
 
-throttle_idle:float = 0.15  # Maximum throttle without generating thrust (throttle floor)
-throttle_max:float = 0.25   # Maximum throttle at 100% input
+idle_throttle_rate:float = 0.15 # Maximum throttle without generating thrust (throttle floor)
+max_throttle_rate:float = 0.25  # Maximum throttle at 100% input
+
+
+##### PID settings #####
+
+max_roll_rate:float = 30.0  # degrees per second
+max_pitch_rate:float = 30.0 # degrees per second
+max_yaw_rate:float = 60.0   # degrees per second
+
+pid_kp_roll:float = 0.00043714285
+pid_kp_pitch:float = 0.00043714285
+pid_kp_yaw:float = 0.001714287
+
+pid_ki_roll:float = 0.00255
+pid_ki_pitch:float = 0.00255
+pid_ki_yaw:float = 0.003428571
+
+pid_kd_roll:float = 0.00002571429
+pid_kd_pitch:float = 0.00002571429
+pid_kd_yaw:float = 0.0
+
+pid_integral_limit_pos:float = 100.0
+pid_integral_limit_neg:float = -pid_integral_limit_pos
 
 
 ##### Constants #####
@@ -32,7 +55,6 @@ IMU_I2C_ADDRESS = 104    # 0x68
 IMU_REG_SMPLRT_DIV = 25  # 0x19
 IMU_REG_PWR_MGMT1 = 107  # 0x6B
 IMU_REG_WHO_AM_I = 117   # 0x75
-
 IMU_REG_CONFIG = 26      # 0x1A
 IMU_REG_GYRO_CONFIG = 27 # 0x1B
 IMU_REG_ACCE_CONFIG = 28 # 0x1C
@@ -51,10 +73,14 @@ IMU_REG_GYRO_Y_LO = 70   # 0x46
 IMU_REG_GYRO_Z_HI = 71   # 0x47
 IMU_REG_GYRO_Z_LO = 72   # 0x48
 
-SCALE_MODIFIER_ACCE:float = 32767/acce_range
-SCALE_MODIFIER_GYRO:float = 32767/gyro_range
+GYRO_INDEX_ROLL = 0
+GYRO_INDEX_PITCH = 1
+GYRO_INDEX_YAW = 2
 
-THROTTLE_RANGE = throttle_max - throttle_idle
+SCALE_MULTIPLIER_ACCE:float = acce_range / 32767
+SCALE_MULTIPLIER_GYRO:float = gyro_range / 32767
+
+THROTTLE_RANGE = max_throttle_rate - idle_throttle_rate
 
 ##### Pin settings #####
 
@@ -72,35 +98,6 @@ MOTOR3_GPIO = Pin(15)
 MOTOR4_GPIO = Pin(16)
 
 
-##### Control variables #####
-
-raw_rc_values:list = [0]*6
-normalised_rc_values = [0]*6
-
-normalised_gyro_values = [0.0]*3
-
-roll_last_integral:float = 0.0
-roll_last_error:float = 0.0
-pitch_last_integral:float = 0.0
-pitch_last_error:float = 0.0
-yaw_last_integral:float = 0.0
-yaw_last_error:float = 0.0
-
-pid_roll_kp:float = 0.00043714285
-pid_roll_ki:float = 0.00255
-pid_roll_kd:float = 0.00002571429
-pid_pitch_kp:float = pid_roll_kp
-pid_pitch_ki:float = pid_roll_ki
-pid_pitch_kd:float = pid_roll_kd
-pid_yaw_kp:float = 0.001714287
-pid_yaw_ki:float = 0.003428571
-pid_yaw_kd:float = 0.0
-
-max_rate_roll:float = 30.0
-max_rate_pitch:float = 30.0
-max_rate_yaw:float = 50.0
-
-
 ##### Enable pin IO #####
 
 rc:UART = UART(1)
@@ -115,13 +112,25 @@ motor4:PWM = PWM(MOTOR4_GPIO)
 
 error_list:list = []
 
+raw_rc_values:list = [0] * 6
+normalised_rc_values:list = [0] * 6
+normalised_gyro_values:list = [0.0] * 3
+
+# This are the starting values that will be used in the first PID calculation
+prev_pid_error_roll:float = 0.0
+prev_pid_error_pitch:float = 0.0
+prev_pid_error_yaw:float = 0.0
+prev_pid_inte_roll:float = 0.0
+prev_pid_inte_pitch:float = 0.0
+prev_pid_inte_yaw:float = 0.0
+
 
 ##### Functions #####
 
 def setup() -> int:
     error_raised_flag:bool = False
 
-    print("INFO >>>>    Starting setup sequence.\n")
+    print("INFO  >>>>   Starting setup sequence.\n")
 
     # Flash LED for 5 seconds
     for i in range(5):
@@ -133,7 +142,7 @@ def setup() -> int:
 
     try:
         freq(250000000)
-        print("INFO >>>>    Overclock RP2040 to 250 MHz --> SUCCESS.\n")
+        print("INFO  >>>>   Overclock RP2040 to 250 MHz --> SUCCESS.\n")
     except:
         error_raised_flag = True
         error_list.append("Unable to overclock RP2040.")
@@ -141,7 +150,7 @@ def setup() -> int:
 
     try:
         rc.init(baudrate=115200, bits=8, parity=None, stop=1, rx=RC_GPIO_IN)
-        print("INFO >>>>    RC receiver setup --> SUCCESS\n")
+        print("INFO  >>>>   RC receiver setup --> SUCCESS\n")
     except:
         error_raised_flag = True
         error_list.append("Unable to initialise UART for RC receiver.")
@@ -151,7 +160,7 @@ def setup() -> int:
         imu.writeto_mem(IMU_I2C_ADDRESS, IMU_REG_PWR_MGMT1, bytes(1))   # Wake command
         imu.writeto_mem(IMU_I2C_ADDRESS, IMU_REG_CONFIG, bytes(5))      # Set accelerometer LPF to 10 Hz
         imu.writeto_mem(IMU_I2C_ADDRESS, IMU_REG_GYRO_CONFIG, bytes(8)) # Set gyroscope scale to 500 dps
-        print("INFO >>>>    MPU-6050 setup --> SUCCESS\n")
+        print("INFO  >>>>   MPU-6050 setup --> SUCCESS\n")
     except:
         # Masking the fail flag here as IMU wiring needs some work
         # IMU SDA and SCL must to be connected to strong pull-up resistors
@@ -162,30 +171,32 @@ def setup() -> int:
     try:
         # Who am I check
         if imu.readfrom_mem(IMU_I2C_ADDRESS, IMU_REG_WHO_AM_I, 1)[0] == IMU_I2C_ADDRESS:
-            print("INFO >>>>   MPU-6050 verify WHO_AM_I -> SUCCESS")
+            print("INFO  >>>>   MPU-6050 verify WHO_AM_I -> SUCCESS\n")
         else:
             error_raised_flag = True
             error_list.append("MPU-6050 WHO_AM_I read error.")
-            print("ERROR >>>>  MPU-6050 verify WHO_AM_I -> FAIL")
+            print("ERROR >>>>   MPU-6050 verify WHO_AM_I -> FAIL\n")
 
         # Low pass filter check
         if imu.readfrom_mem(IMU_I2C_ADDRESS, IMU_REG_CONFIG, 1)[0] == 5:
-            print("INFO >>>>    MPU-6050 verify DLPF -> SUCCESS")
+            print("INFO  >>>>   MPU-6050 verify DLPF -> SUCCESS\n")
         else:
             error_raised_flag = True
             error_list.append("MPU-6050 DLPF not set.")
-            print("ERROR >>>>  MPU-6050 verify DLPF -> FAIL")
+            print("ERROR >>>>   MPU-6050 verify DLPF -> FAIL\n")
     except:
-        error_raised_flag = True
+        # Masking the fail flag here as IMU wiring needs some work
+        # IMU SDA and SCL must to be connected to strong pull-up resistors
+        # error_raised_flag = True
         error_list.append("IMU I2C read error.")
-        print("ERROR >>>>  MPU-6050 verify settings -> FAIL")
+        print("ERROR >>>>   MPU-6050 verify settings -> FAIL\n")
 
     try:
         motor1.freq(250)
         motor2.freq(250)
         motor3.freq(250)
         motor4.freq(250)
-        print("INFO >>>>    Set motor PWM freq to 250 Hz --> SUCCESS\n")
+        print("INFO  >>>>   Set motor PWM freq to 250 Hz --> SUCCESS\n")
     except:
         error_raised_flag = True
         error_list.append("Unable to set motor PWM freq to 250 Hz.")
@@ -227,14 +238,15 @@ def rc_read() -> None:
                     # Validate checksum
                     if checksum == (buffer[29] << 8) | buffer[28]: # Converting to big endian
                         for channel in range(6):
-                            raw_rc_values[channel] = (buffer[channel*2 + 1] << 8) + buffer[channel*2]
+                            raw_rc_values[channel] = (buffer[channel*2 + 1] << 8) + buffer[channel * 2]
 
                         break
 
     # Normalise data
     for index in range(6):
-        # normalised_rc_values[index] = float(raw_rc_values[index]/1000 - 1) # From 1000-2000 to 0.0-1.0
-        normalised_rc_values[index] = raw_rc_values[index]*1000 # From 1000-2000 to 1000000-2000000
+        # Normalise to 0.0-1.0 as these values are scaled with respect to the maximum rates defined in settings
+        # normalised_rc_values[index] = float(raw_rc_values[index]*0.001 - 1) # From 1000-2000 to 0.0-1.0
+        normalised_rc_values[index] = raw_rc_values[index] * 1000 # From 1000-2000 to 1000000-2000000
 
 
 def imu_read() -> None:
@@ -247,23 +259,25 @@ def imu_read() -> None:
     raw_gyro_data = imu.readfrom_mem(IMU_I2C_ADDRESS, IMU_REG_GYRO_X_HI, 6)
 
     for axis in range(3):
-        value = (raw_gyro_data[axis*2] << 8) | raw_gyro_data[axis*2 + 1]
+        value = (raw_gyro_data[axis * 2] << 8) | raw_gyro_data[axis*2 + 1]
 
         # Check if negative
-        if raw_gyro_data[axis*2] >= 0xFF:
-            normalised_gyro_values[axis] = -((0xFFFF - value) + 1) / SCALE_MODIFIER_GYRO
+        if raw_gyro_data[axis * 2] >= 0xFF:
+            normalised_gyro_values[axis] = -((0xFFFF - value) + 1) * SCALE_MULTIPLIER_GYRO
         else:
-            normalised_gyro_values[axis] = value / SCALE_MODIFIER_GYRO
+            normalised_gyro_values[axis] = value * SCALE_MULTIPLIER_GYRO
 
 
 ##### Main #####
 
 if setup() == 0:
-    while True:
-        # start_time = time.ticks_us()
+    prev_pid_timestamp:float = time.ticks_us()
+    print("INFO  >>>>   Starting flight control loop.\n")
 
+    while True:
         try:
             rc_read()
+            # imu_read()
             print(normalised_rc_values)
 
             # Just simple throttle testing for now
@@ -272,19 +286,71 @@ if setup() == 0:
             motor3.duty_ns(normalised_rc_values[RC_THROTTLE_CH])
             motor4.duty_ns(normalised_rc_values[RC_THROTTLE_CH])
 
-            # imu_read()
-            desired_throttle:float = normalised_rc_values[RC_THROTTLE_CH]
-            desired_pitch:float = normalised_rc_values[RC_PITCH_CH]
-            desired_roll:float = normalised_rc_values[RC_ROLL_CH]
-            desired_yaw:float = normalised_rc_values[RC_YAW_CH]
+            desired_throttle_rate:float = normalised_rc_values[RC_THROTTLE_CH]
+            desired_pitch_rate:float = normalised_rc_values[RC_PITCH_CH]
+            desired_roll_rate:float = normalised_rc_values[RC_ROLL_CH]
+            desired_yaw_rate:float = normalised_rc_values[RC_YAW_CH]
 
-            adjust_throttle:float = THROTTLE_RANGE*desired_throttle + throttle_idle
+            # Error calculations (desired - actual)
+            pid_error_roll:float = desired_roll_rate*max_roll_rate - normalised_gyro_values[GYRO_INDEX_ROLL]
+            pid_error_pitch:float = desired_pitch_rate*max_pitch_rate - normalised_gyro_values[GYRO_INDEX_PITCH]
+            pid_error_yaw:float = desired_yaw_rate*max_yaw_rate - normalised_gyro_values[GYRO_INDEX_YAW]
+
+            # Proportion calculations
+            pid_prop_roll:float = pid_error_roll * pid_kp_roll
+            pid_prop_pitch:float = pid_error_pitch * pid_kp_pitch
+            pid_prop_yaw:float = pid_error_yaw * pid_kp_yaw
+
+            # Calculate time elapsed since previous PID calculations
+            pid_cycle_time:float = time.ticks_diff(time.ticks_us(), prev_pid_timestamp) * 0.000001
+
+            # Integral calculations
+            pid_inte_roll:float = pid_error_roll * pid_ki_roll * pid_cycle_time + prev_pid_inte_roll
+            pid_inte_pitch:float = pid_error_pitch * pid_ki_pitch * pid_cycle_time + prev_pid_inte_pitch
+            pid_inte_yaw:float = pid_error_yaw * pid_ki_yaw * pid_cycle_time + prev_pid_inte_yaw
+
+            # Constrain within integral limits
+            pid_inte_roll = max(min(pid_inte_roll, pid_integral_limit_pos), pid_integral_limit_neg)
+            pid_inte_pitch = max(min(pid_inte_pitch, pid_integral_limit_pos), pid_integral_limit_neg)
+            pid_inte_yaw = max(min(pid_inte_yaw, pid_integral_limit_pos), pid_integral_limit_neg)
+
+            # Derivative calculations
+            pid_deri_roll:float = (pid_error_roll - prev_pid_error_roll) * pid_kd_roll * pid_cycle_time
+            pid_deri_pitch:float = (pid_error_pitch - prev_pid_error_pitch) * pid_kd_pitch * pid_cycle_time
+            pid_deri_yaw:float = (pid_error_yaw - prev_pid_error_yaw) * pid_kd_yaw * pid_cycle_time
+
+            # Capture end timestamp for current PID loop
+            prev_pid_timestamp = time.ticks_us()
+
+            adjust_throttle:float = THROTTLE_RANGE*desired_throttle_rate + idle_throttle_rate
+            pid_roll:float = pid_prop_roll + pid_inte_roll + pid_deri_roll
+            pid_pitch:float = pid_prop_pitch + pid_inte_pitch + pid_deri_pitch
+            pid_yaw:float = pid_prop_yaw + pid_inte_yaw + pid_deri_yaw
+
+            # Throttle calculations (cross configuration)
+            motor1_throttle:float = adjust_throttle + pid_roll + pid_pitch - pid_yaw
+            motor2_throttle:float = adjust_throttle - pid_roll + pid_pitch + pid_yaw
+            motor3_throttle:float = adjust_throttle + pid_roll - pid_pitch + pid_yaw
+            motor4_throttle:float = adjust_throttle - pid_roll - pid_pitch - pid_yaw
+
+            # Save PID values for subsequent calculations
+            prev_pid_error_roll = 0.0
+            prev_pid_error_pitch = 0.0
+            prev_pid_error_yaw = 0.0
+            prev_pid_inte_roll = 0.0
+            prev_pid_inte_pitch = 0.0
+            prev_pid_inte_yaw = 0.0
+
+            # Calculate duty cycle
+
         except:
             break
 
-        # print("INFO >>>>    Loop time:", time.ticks_diff(time.ticks_us(), start_time))
+        # print("INFO  >>>>   Loop duration:", pid_cycle_time) # Target duration is less than 0.004 s
+
+    # Out of while loop
 else:
-    print("ERROR >>>   Setup failed.\n")
+    print("ERROR >>>>   Setup failed.\n")
 
     for error in error_list:
         print("ERROR >>>>  ", error)

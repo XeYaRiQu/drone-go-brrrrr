7d02f7f2a6960a6ab0c2b26f09e6dee81cef6555
XeYaRiQu <e0564833@u.nus.edu>
2024-01-25 16:35:50 +0800

    Add starter code

--- pi-pico-w/c/test/main.c
+++ pi-pico-w/c/test/main.c
@@ -1,5 +1,44 @@
-#include <stdio.h>
+/* C implementation for faster run times.
+Fallback to MicroPython if necessary.*/
 
-void main() {
+////////////////// Includes //////////////////
+
+#include <stdio.h>
+#include <Wire.h>
+
+
+////////////////// Settings //////////////////
+
+int gyro_range = 250;
+int acce_range = 4;
+
+
+
+
+////////////////// PID settings //////////////////
+
+
+
+////////////////// Defines //////////////////
+
+
+
+
+////////////////// Constants //////////////////
+
+
+
+
+////////////////// Global variables //////////////////
+
+
+
+
+int setup() {
+
+}
+
+
+void loop() {
     printf("Hello World!");
 }

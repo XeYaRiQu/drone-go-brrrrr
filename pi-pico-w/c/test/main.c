780ed28456684a9afba033c32a0f2b5fa9c62b9c
XeYaRiQu <142389628+XeYaRiQu@users.noreply.github.com>
2024-01-26 02:56:10 +0800

    Add

--- pi-pico-w/c/test/main.c
+++ pi-pico-w/c/test/main.c
@@ -1,46 +1,5 @@
-/* C implementation for faster run times.
-Fallback to MicroPython if necessary.*/
-
-////////////////// Includes //////////////////
-
 #include <stdio.h>
-#include <Wire.h>
 
-
-////////////////// Settings //////////////////
-
-int gyro_range = 250;
-int acce_range = 4;
-
-
-
-
-////////////////// PID settings //////////////////
-
-
-
-////////////////// Defines //////////////////
-
-
-
-
-////////////////// Constants //////////////////
-
-
-
-
-////////////////// Global variables //////////////////
-
-
-
-
-int setup() {
-
-}
-
-
-void loop() {
-    unsigned long loop_start_timestamp = micros();
+void main() {
     printf("Hello World!");
-    while (micros() - loop_start_timestamp < 4000000); // Do nothing until 4 ms has passed since loop start
 }
